a11f12424e62395499b3c49f3f2dc654693b84e4
Jeffrey Petersen <jeffreybpetersen@gmail.com>
2015-02-14 14:03:30 -0800

    blue_triangle_wave light option

--- lampient.js
+++ lampient.js
@@ -1,4 +1,44 @@
+// takes rgb values from 0 to 255
+function set_color(r,g,b){
+  document.body.style.backgroundColor = 
+    "rgb(" + Math.floor(r) + "," + Math.floor(g) + "," + Math.floor(b) + ")"
+}
+
+// begins ticking
+function tick(){
+  ticks++
+  time += 1/max_fps
+  //console.log("ticks: " + ticks + ", time: " + time)
+  light()
+  setTimeout(function(){tick()}, 1000/max_fps)
+}
+
+// light options
+
+function off(){
+  set_color(0,0,0)
+}
+
+function blue_triangle_wave(){
+  var t = time % 2
+  x = t % 2 > t % 1 ? t - (t % 1) * 2 : t
+  set_color(0,0,Math.floor(255 * x))
+  console.log(x)
+}
+
+// initializes lampient
 function init(){
-  console.log("Hello Lampient")
-  document.body.style.backgroundColor = "blue"
+  // welcome to the lampient codebase
+  console.log("Welcome to Lampient")
+  
+  // initialize global variables
+  max_fps = 60
+  //  different time based variables
+  ticks = 0 // ticks passed
+  time = 0 // seconds passed at max_fps
+  //  light off at start
+  light = off
+  
+  // start ticking
+  tick()
 }
