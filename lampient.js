635262bbf4d8538573f94e464918ccc57f429380
Jeffrey Petersen <jeffreybpetersen@gmail.com>
2015-04-28 23:44:35 -0700

    add options

--- lampient.js
+++ lampient.js
@@ -9,21 +9,54 @@ function tick(){
   ticks++
   time += 1/max_fps
   //console.log("ticks: " + ticks + ", time: " + time)
-  light()
+  light.main()
   setTimeout(function(){tick()}, 1000/max_fps)
+}
+
+function set_light(light_option){
+  light_option.init()
+  light = light_option
 }
 
 // light options
 
-function off(){
-  set_color(0,0,0)
+var off = {
+  main: function(){
+    set_color(0,0,0)
+  }
 }
 
-function blue_triangle_wave(){
-  var t = time % 2
-  x = t % 2 > t % 1 ? t - (t % 1) * 2 : t
-  set_color(0,0,Math.floor(255 * x))
-  console.log(x)
+var aidan_alarm = {
+  //dedicated to my roommate Aidan, who suggested this effect
+  init: function(){},
+  main: function(){
+    var ticks_per_cycle = 4
+    Math.floor(ticks % ticks_per_cycle) < ticks_per_cycle / 2 ? set_color(255,255,0) : set_color(255,0,0)
+  }
+}
+
+var blue_triangle_wave = {
+  init: function(){},
+  main: function(){
+    var t = time % 2
+    var x = t % 2 > t % 1 ? t - (t % 1) * 2 : t
+    set_color(0,0,Math.floor(255 * x))
+    console.log(x)
+  }
+}
+
+var candle = {
+  init: function(){
+    this.dist = 64
+    this.base = 255 - this.dist
+    this.diff = 0
+  },
+  main: function(){
+    var level = this.base + this.diff
+    this.diff += Math.floor(Math.random()*3/2)
+    this.diff = this.diff % this.dist
+    set_color(level,level,level)
+  }
 }
 
 // initializes lampient
