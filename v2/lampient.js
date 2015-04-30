b057248026be9f80c87d4dee00e52d35796c8190
Jeffrey Petersen <jeffreybpetersen@gmail.com>
2015-04-30 11:30:26 -0700

    add filling lamp by color

--- v2/lampient.js
+++ v2/lampient.js
@@ -1,3 +1,8 @@
+function fill_color(color){
+  lamp_context.fillStyle = color
+  lamp_context.fillRect(0, 0, lamp_element.width, lamp_element.height)
+}
+
 function fullscreen(){
   lamp_element.height = screen.height
   lamp_element.width = screen.width
@@ -6,4 +11,5 @@ function fullscreen(){
 
 function init(){
   lamp_element = document.getElementById("lamp")
+  lamp_context = lamp_element.getContext("2d")
 }
