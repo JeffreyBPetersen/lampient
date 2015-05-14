e9f7f714f5c3b6b460a771127ea9bf66d308607d
Jeffrey Petersen <jeffreybpetersen@gmail.com>
2015-05-14 00:30:53 -0700

    add timed loop using requestAnimationFrame

--- v2/lampient.js
+++ v2/lampient.js
@@ -20,9 +20,19 @@ function handle_keydown(event){
   }
 }
 
+// initialize with requestAnimationFrame(loop) to avoid undefined time
+function loop(time){
+  requestAnimationFrame(loop)
+  console.log(time/1000)
+}
+
 function init(){
+  // initialize canvas variables
   lamp_element = document.getElementById("lamp")
+  lamp_context = lamp_element.getContext("2d")
+  // initialize input listeners
   lamp_element.addEventListener("click", function(){handle_click(event)})
   lamp_element.addEventListener("keydown", function(){handle_keydown(event)})
-  lamp_context = lamp_element.getContext("2d")
+  // begin main loop
+  requestAnimationFrame(loop)
 }
